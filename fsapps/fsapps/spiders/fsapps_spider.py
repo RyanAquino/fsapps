6782884d30bab74f12f1ef53b4cb62b480c38789
ryanaquino <ryan@extrac.io>
2023-02-24 18:54:57 +0800

    Added year/quarter support

--- fsapps/fsapps/spiders/fsapps_spider.py
+++ fsapps/fsapps/spiders/fsapps_spider.py
@@ -6,7 +6,19 @@ from scrapy.http import Request
 
 class FsappsSpider(scrapy.Spider):
     name = "fsapps"
-    start_urls = ["https://fsapps.fiscal.treasury.gov/dts/issues"]
+    year = 2023
+    start_urls = [
+        f"https://fsapps.fiscal.treasury.gov/dts/issues/{year}" for year in range(1998, year+1)
+    ]
+
+    def __init__(self):
+        quarter_per_year = []
+        for url in self.start_urls:
+            for ctr in range(1, 5):
+                quarter_per_year.append(f"{url}/{ctr}")
+
+        self.start_urls += quarter_per_year
+        super().__init__()
 
     def parse(self, response):
         for link in response.css("a::attr(href)"):
