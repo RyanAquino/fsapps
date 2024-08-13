9166278af036f41a1b54a4c73beba777fa29bf2d
RyanAquino <ryan.aquino7110@gmail.com>
2024-08-13 18:50:57 +0800

    Add CORS

--- proj/a2/api/main.py
+++ proj/a2/api/main.py
@@ -2,13 +2,21 @@ import uvicorn
 from config import settings
 from fastapi import FastAPI
 from routers.router import api_router
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.database import Base, engine
 
 
 def create_app():
     app = FastAPI()
     app.include_router(api_router, prefix="/api/v1")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     Base.metadata.create_all(engine, checkfirst=True)
 
     return app
