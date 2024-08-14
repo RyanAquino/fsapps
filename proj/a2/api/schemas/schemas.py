1f0f4c35d23df7b363db34c97d5678ece82e131b
RyanAquino <ryan.aquino7110@gmail.com>
2024-08-14 23:45:03 +0800

    Dev: Add operating cash balance - net change field on response

--- proj/a2/api/schemas/schemas.py
+++ proj/a2/api/schemas/schemas.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
@@ -9,3 +11,26 @@ class UserDBSchema(BaseModel):
     class Config:
         from_attributes = True
         extra = "allow"
+
+
+class OperatingCashBalanceDBSchema(BaseModel):
+    id: int
+    record_date: str
+    account_type: str
+    close_today_bal: str
+    open_today_bal: int
+    open_month_bal: int
+    open_fiscal_year_bal: int
+    table_nbr: str
+    table_nm: str
+    sub_table_name: str
+    src_line_nbr: str
+    record_fiscal_year: int
+    record_fiscal_quarter: int
+    record_calendar_year: int
+    record_calendar_quarter: int
+    record_calendar_month: int
+    record_calendar_day: int
+
+    class Config:
+        from_attributes = True
--- proj/a2/api/services/operating_cash_balance_service.py
+++ proj/a2/api/services/operating_cash_balance_service.py
@@ -1,4 +1,5 @@
 from api.repositories.dts_tables_repository import DTSTablesBaseRepository
+from api.schemas.schemas import OperatingCashBalanceDBSchema
 from api.services.dts_table_base_service import DTSBaseService
 
 
@@ -7,5 +8,15 @@ class OperatingCashBalanceService(DTSBaseService):
         super().__init__(dts_table_repository, service)
 
     def get_data(self, filters):
-        # Business logic here
-        return self._repository.get_all()
+        data = self._repository.get_all()
+        serialized_items = [
+            OperatingCashBalanceDBSchema.model_validate(item) for item in data if item.close_today_bal != "null"
+        ]
+        json_data = []
+
+        for item in serialized_items:
+            item_data = item.model_dump()
+            item_data["net_change"] = int(item.open_today_bal) - int(item.close_today_bal)
+            json_data.append(item_data)
+
+        return json_data
