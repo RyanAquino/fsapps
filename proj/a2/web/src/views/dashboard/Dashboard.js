37301dd61190cd8b99283f4f81ce897bbca4e6ea
RyanAquino <ryan.aquino7110@gmail.com>
2024-08-20 19:45:17 +0800

    Dev[FE]: Adjust chart size

--- proj/a2/web/src/views/dashboard/Dashboard.js
+++ proj/a2/web/src/views/dashboard/Dashboard.js
@@ -15,7 +15,7 @@ const Dashboard = () => {
     <PageContainer title="Dashboard" description="this is Dashboard">
       <Box>
         <Grid container spacing={3}>
-          <Grid item xs={12} lg={6}>
+          <Grid item xs={12} lg={12}>
             <SalesOverview />
           </Grid>
           {/*<Grid item xs={12} lg={4}>*/}
