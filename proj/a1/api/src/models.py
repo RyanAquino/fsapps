e89a33ec0287d46e17c20afbe538586de032b857
RyanAquino <ryan.aquino7110@gmail.com>
2024-08-31 00:40:55 +0800

    Dev[BE]: Reduce DB pool recycle to 5 min

--- proj/a1/api/src/models.py
+++ proj/a1/api/src/models.py
@@ -278,7 +278,7 @@ def init_db():
         with open("/run/secrets/db_conn") as f:
             conn = f.readlines()[0]
 
-    engine = create_engine(conn, pool_recycle=3600, pool_pre_ping=True)
+    engine = create_engine(conn, pool_recycle=300, pool_pre_ping=True)
     # disable for prod, user has only select and insert privileges
     Base.metadata.create_all(engine)
 
